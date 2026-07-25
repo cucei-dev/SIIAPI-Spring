d6e8ef1fb0eb91afd4e4ad6c2e2b01228feb3c80
milosnowcat <ramiro.alvarez@rahcode.com>
2026-07-25 13:10:39 -0600

    init: spring

---
+++ src/main/java/dev/cucei/siiapi/SiiapiApplication.java
@@ -0,0 +1,13 @@
+package dev.cucei.siiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cucei/siiapi/SiiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cucei.siiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
